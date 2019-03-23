cf1c4f980fb542dce58dce371cca0674720829fe
IgorPyat <fasthunter137@gmail.com>
2019-03-23 13:06:46 +0500

    lesson

--- server_files/js/funcs/sqlite.func.js
+++ server_files/js/funcs/sqlite.func.js
@@ -7,29 +7,41 @@ var db = new sqlite3.Database('server_files/db/Students.db', function(err){
     console.log('База подключена');
 })
 
+function dbGetUsers(err, rows, res) {
+    if (err) {
+        console.log('Ошибка запроса', err.message);
+        res.send({
+            status: 'success',
+            message: 'Ошибка',
+        });
+        return;
+    }
+    res.send({
+        status: 'success',
+        message: 'Данные',
+        data: rows
+    });
+}
+
 
 function Vkrs(res) {
 
-    var sql ='select Users.Name as ФИО, Vkrs.Name as ВКР from Users join Vkrs on Users.VkrId = Vkrs.Id ';
-    db.all(sql, (err, rows) => setRes(err,rows,res,Name));
-    db.close() 
+    const sql ='select Users.Name as ФИО, Vkrs.Name as ВКР from Users join Vkrs on Users.VkrId = Vkrs.Id ';
+    db.all(sql, function(err, rows) {
+        dbGetUsers(err, rows, res);
+    });
 }
     
 function User(req, res, next) {
 
-    var Name = req.params.name;
-    var sql ='Select Vkrs.Name from Vkrs join Users on Users.Vkrid = Vkrs.Id where Users.name = ' + Name;
-    db.all(sql, (err, rows) => setRes(err,rows,res,Name));
-    db.close() 
-}
-    
+    var Name = req.params.user;
 
-function setRes(rows,res){
-    res.send({
-        status: 'success',
-        data: rows
+    const sql ='Select Vkrs.Name from Vkrs join Users on Users.Vkrid = Vkrs.Id where Users.name = "' + Name+'"';
+    db.all(sql, function(err, rows) {
+        dbGetUsers(err, rows, res);
     });
 }
+
 
 
 
